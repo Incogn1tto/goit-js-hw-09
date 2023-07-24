02c198e9ce558e5e3cd69a0c0fdeb2b8ac019200
Incogn1tto <ageenkopetr48@gmail.com>
2023-07-24 05:09:29 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,6 +2,8 @@ const startBtnEl = document.querySelector('[data-start]');
 const stopBtnEl = document.querySelector('[data-stop]');
 stopBtnEl.disabled = true;
 
+let intervalId;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
